6ca560f7a74b9d7e7555211dfb2f68274a55c0b9
DJLGabJean <116765613+DJLGabJean@users.noreply.github.com>
2024-11-05 23:31:21 +0100

    Ajout de la fonctionnalité de la caméra et de la galerie photo + ajout du dossier android

--- bricoscope/src/app/tab1/tab1.page.ts
+++ bricoscope/src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
 @Component({
   selector: 'app-tab1',
@@ -9,4 +10,18 @@ export class Tab1Page {
 
   constructor() {}
 
+  selectedImage: string | undefined;
+
+  async importFromGallery() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Photos
+    });
+    this.selectedImage = image.webPath;
+    console.log('Image importée:', image.webPath);
+  }
 }
+
+
--- bricoscope/src/app/tab2/tab2.page.ts
+++ bricoscope/src/app/tab2/tab2.page.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { CameraPreview, CameraPreviewOptions } from '@capacitor-community/camera-preview';
+
+const cameraPreviewOptions: CameraPreviewOptions = {
+  position: 'rear',
+  height: 1920,
+  width: 1080
+};
+CameraPreview.start(cameraPreviewOptions);
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +14,46 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  image: string | null = null;
+  cameraActive = false;
+  loading = false;
 
   constructor() {}
 
+  openCamera() {
+    const cameraPreviewOptions: CameraPreviewOptions = {
+      position: 'rear',
+      parent : 'cameraPreview',
+      className : 'cameraPreview',
+    };
+    this.loading = true;
+    setTimeout(() => {
+      CameraPreview.start(cameraPreviewOptions);
+      this.loading = false;
+    }, 1500);
+    this.cameraActive = true;
+  }
+
+  async stopCamera() {
+    CameraPreview.stop();
+    this.cameraActive = false;
+  }
+
+  async captureImage() {
+    const cameraPreviewPictureOptions = {
+      quality: 90
+    };
+
+    const result = await CameraPreview.capture({ quality: 90 });
+    this.image = `data:image/jpeg;base64,${result.value}`;
+    this.stopCamera();
+  }
+
+  flipCamera() {
+    CameraPreview.flip();
+  }
+
+  clearImage() {
+    this.image = null;
+  }
 }
