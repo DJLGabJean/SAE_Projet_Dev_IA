364c501da6f0abb9d7dcadafc5b485595220213c
DJLGabJean <116765613+DJLGabJean@users.noreply.github.com>
2024-12-02 23:35:34 +0100

    Ajout de l'API Firebase + Ajout du bouton de sauvegarde d'image sur le tab1 (wip)

--- bricoscope/src/environments/environment.ts
+++ bricoscope/src/environments/environment.ts
@@ -1,19 +1,12 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyChuPXWr7FQtAHlu7_O_n62SnEtwnLwOrc",
+    authDomain: "yolov8api.firebaseapp.com",
+    projectId: "yolov8api",
+    storageBucket: "yolov8api.firebasestorage.app",
+    messagingSenderId: "769406795959",
+    appId: "1:769406795959:web:19b106dcb4786a1fc6cd32",
+    measurementId: "G-MLT12SND4E"
+  },
 };
-
-const firebaseConfig = {
-  apiKey: "AIzaSyChuPXWr7FQtAHlu7_O_n62SnEtwnLwOrc",
-  authDomain: "yolov8api.firebaseapp.com",
-  projectId: "yolov8api",
-  storageBucket: "yolov8api.firebasestorage.app",
-  messagingSenderId: "769406795959",
-  appId: "1:769406795959:web:19b106dcb4786a1fc6cd32",
-  measurementId: "G-MLT12SND4E"
-}
-
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
